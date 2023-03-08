3d09497d26aa479570fe2674ac6bd9fe942c19a5
Jon Hadfield <jon@lessknown.co.uk>
2023-03-08 20:22:18 +0000

    export GitLab DefaultMinimumProjectAccessLevel for library importers to reference.

--- gitlab.go
+++ gitlab.go
@@ -18,7 +18,7 @@ import (
 )
 
 const (
-	DefaultMinimumProjectAccessLevel = 20
+	defaultMinimumProjectAccessLevel = 20
 )
 
 type gitlabHost struct {
@@ -151,7 +151,7 @@ func (provider gitlabHost) getAllProjectRepositories(client http.Client) (repos 
 			logger.Printf("GITLAB_PROJECT_MIN_ACCESS_LEVEL '%s' is not a number so using default",
 				minAccessLevelEnvVar)
 
-			minAccessLevel = DefaultMinimumProjectAccessLevel
+			minAccessLevel = defaultMinimumProjectAccessLevel
 		}
 	}
 
@@ -161,7 +161,7 @@ func (provider gitlabHost) getAllProjectRepositories(client http.Client) (repos 
 				strings.Join(validMinimumProjectAccessLevels, ", "))
 		}
 
-		minAccessLevel = DefaultMinimumProjectAccessLevel
+		minAccessLevel = defaultMinimumProjectAccessLevel
 	}
 
 	logger.Printf("project minimum access level set to %s (%d)",
