ea76978ac33cb01afd7b00202514f4a0820eb24c
Jon Hadfield <jon@lessknown.co.uk>
2026-04-17 19:34:21 +0100

    support for bitbucket api changes.

--- bitbucket.go
+++ bitbucket.go
@@ -65,6 +65,7 @@ type NewBitBucketHostInput struct {
 	LogLevel             int
 	BackupLFS            bool
 	EncryptionPassphrase string
+	Workspaces           []string
 }
 
 func NewBitBucketHost(input NewBitBucketHostInput) (*BitbucketHost, error) {
@@ -113,6 +114,7 @@ func NewBitBucketHost(input NewBitBucketHostInput) (*BitbucketHost, error) {
 		Key:                  input.Key,
 		Secret:               input.Secret,
 		EncryptionPassphrase: input.EncryptionPassphrase,
+		Workspaces:           input.Workspaces,
 	}
 
 	// If key and secret are provided, get OAuth token
@@ -261,125 +263,84 @@ func urlWithBasicAuth(httpsURL, user, password string) string {
 	return fmt.Sprintf("%s//%s:%s@%s", parts[0], user, password, parts[1])
 }
 
-func (bb BitbucketHost) describeRepos() (describeReposOutput, errors.E) {
-	logger.Println("listing BitBucket repositories")
-
-	var err error
-
-	var repos []repository
-
-	if bb.AuthType != AuthTypeBitbucketOAuth2 && bb.AuthType != AuthTypeBitbucketAPIToken {
-		return describeReposOutput{}, errors.New("no authentication method available - need either OAuth key/secret or API token/email")
+// bitbucketAuthenticatedGet performs an authenticated GET request to the Bitbucket API,
+// returning the response body. It handles both API token and OAuth2 bearer token auth.
+func (bb BitbucketHost) bitbucketAuthenticatedGet(ctx context.Context, rawURL string) ([]byte, errors.E) {
+	req, err := retryablehttp.NewRequestWithContext(ctx, http.MethodGet, rawURL, nil)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to create new request")
 	}
+
+	u, parseErr := url.Parse(rawURL)
+	if parseErr != nil {
+		return nil, errors.Wrap(parseErr, "failed to parse request URL")
+	}
+
+	req.URL = u
+
+	switch bb.AuthType {
+	case AuthTypeBitbucketAPIToken:
+		req.SetBasicAuth(bb.Email, bb.APIToken)
+	case AuthTypeBearerToken:
+		req.Header.Set(HeaderAuthorization, AuthPrefixBearer+bb.OAuthToken)
+	}
+
+	req.Header.Set(HeaderContentType, contentTypeApplicationJSON)
+	req.Header.Set(HeaderAccept, contentTypeApplicationJSON)
+
+	resp, doErr := bb.HttpClient.Do(req)
+	if doErr != nil {
+		return nil, errors.Wrap(doErr, "failed to make request")
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		logger.Printf("unexpected status code: %s (%d)", resp.Status, resp.StatusCode)
+
+		return nil, errors.Errorf("unexpected status code: %s (%d)", resp.Status, resp.StatusCode)
+	}
+
+	bodyB, readErr := io.ReadAll(resp.Body)
+	if readErr != nil {
+		return nil, errors.Errorf("failed to read response body: %s", readErr)
+	}
+
+	return bytes.ReplaceAll(bodyB, []byte("\r"), []byte("\r\n")), nil
+}
+
+// getWorkspaces returns the list of workspace slugs to query for repositories.
+// If explicit workspaces were configured, those are returned directly.
+// Otherwise, it auto-discovers workspaces via the Bitbucket API.
+func (bb BitbucketHost) getWorkspaces() ([]string, errors.E) {
+	if len(bb.Workspaces) > 0 {
+		logger.Printf("using %d configured BitBucket workspace(s)", len(bb.Workspaces))
+
+		return bb.Workspaces, nil
+	}
+
+	logger.Println("discovering BitBucket workspaces")
 
 	ctx, cancel := context.WithTimeout(context.Background(), defaultHttpRequestTimeout)
 	defer cancel()
 
-	rawRequestURL := bb.APIURL + "/repositories?role=member"
+	var workspaces []string
+
+	rawRequestURL := bb.APIURL + "/workspaces?role=member"
 
 	for {
-		req, errNewReq := retryablehttp.NewRequestWithContext(ctx, http.MethodGet, rawRequestURL, nil)
-		if errNewReq != nil {
-			logger.Println(errNewReq)
-
-			return describeReposOutput{}, errors.Wrap(errNewReq, "failed to create new request")
-		}
-
-		var requestUrl string
-
-		switch bb.AuthType {
-		case AuthTypeBitbucketAPIToken:
-			req.SetBasicAuth(bb.Email, bb.APIToken)
-
-			requestUrl = rawRequestURL
-
-			var u *url.URL
-
-			u, err = url.Parse(requestUrl)
-			if err != nil {
-				logger.Println(err)
-
-				return describeReposOutput{}, errors.Wrap(err, "failed to parse request URL")
-			}
-
-			req.URL = u
-		case AuthTypeBearerToken:
-			// if it's auth url, then it's the API token
-			requestUrl = rawRequestURL
-
-			var u *url.URL
-
-			u, err = url.Parse(requestUrl)
-			if err != nil {
-				logger.Println(err)
-
-				return describeReposOutput{}, errors.Wrap(err, "failed to parse request URL")
-			}
-
-			req.URL = u
-			req.Header.Set(HeaderAuthorization, AuthPrefixBearer+bb.OAuthToken)
-		}
-
-		req.Method = http.MethodGet
-		req.Header.Set(HeaderContentType, contentTypeApplicationJSON)
-		req.Header.Set(HeaderAccept, contentTypeApplicationJSON)
-
-		var resp *http.Response
-
-		resp, err = bb.HttpClient.Do(req)
+		body, err := bb.bitbucketAuthenticatedGet(ctx, rawRequestURL)
 		if err != nil {
-			logger.Println(err)
-
-			return describeReposOutput{}, errors.Wrap(err, "failed to make request")
+			return nil, errors.Wrap(err, "failed to list BitBucket workspaces")
 		}
 
-		if resp.StatusCode != http.StatusOK {
-			logger.Printf("unexpected status code: %s (%d)", resp.Status, resp.StatusCode)
-
-			_ = resp.Body.Close()
-
-			return describeReposOutput{}, errors.Errorf("unexpected status code: %s (%d)", resp.Status, resp.StatusCode)
+		var respObj bitbucketGetWorkspacesResponse
+		if jErr := json.Unmarshal(body, &respObj); jErr != nil {
+			return nil, errors.Wrap(jErr, "failed to unmarshal BitBucket workspaces response")
 		}
 
-		var bodyB []byte
-
-		bodyB, err = io.ReadAll(resp.Body)
-		if err != nil {
-			return describeReposOutput{}, errors.Errorf("failed to read response body: %s", err)
-		}
-
-		bodyStr := string(bytes.ReplaceAll(bodyB, []byte("\r"), []byte("\r\n")))
-		_ = resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			var errResp bitbucketErrorResponse
-			if err = json.Unmarshal([]byte(bodyStr), &errResp); err != nil {
-				logger.Println(err)
-
-				return describeReposOutput{}, errors.Wrap(err, "failed to unmarshall bitbucket error json response")
-			}
-
-			return describeReposOutput{}, errors.Errorf("bitbucket request failed: %s", errResp.Error.Message)
-		}
-
-		var respObj bitbucketGetProjectsResponse
-		if err = json.Unmarshal([]byte(bodyStr), &respObj); err != nil {
-			logger.Println(err)
-
-			return describeReposOutput{}, errors.Wrap(err, "failed to unmarshall bitbucket json response")
-		}
-
-		for _, r := range respObj.Values {
-			if r.Scm == "git" {
-				repo := repository{
-					Name:              r.Name,
-					HTTPSUrl:          "https://bitbucket.org/" + r.FullName + ".git",
-					PathWithNameSpace: r.FullName,
-					Domain:            bitbucketDomain,
-				}
-
-				repos = append(repos, repo)
-			}
+		for _, w := range respObj.Values {
+			workspaces = append(workspaces, w.Slug)
 		}
 
 		if respObj.Next != "" {
@@ -389,6 +350,71 @@ func (bb BitbucketHost) describeRepos() (describeReposOutput, errors.E) {
 		}
 
 		break
+	}
+
+	if len(workspaces) == 0 {
+		return nil, errors.New("no BitBucket workspaces found for the authenticated user")
+	}
+
+	logger.Printf("found %d BitBucket workspace(s)", len(workspaces))
+
+	return workspaces, nil
+}
+
+func (bb BitbucketHost) describeRepos() (describeReposOutput, errors.E) {
+	logger.Println("listing BitBucket repositories")
+
+	if bb.AuthType != AuthTypeBitbucketOAuth2 && bb.AuthType != AuthTypeBitbucketAPIToken {
+		return describeReposOutput{}, errors.New("no authentication method available - need either OAuth key/secret or API token/email")
+	}
+
+	workspaces, wsErr := bb.getWorkspaces()
+	if wsErr != nil {
+		return describeReposOutput{}, errors.Wrap(wsErr, "failed to get BitBucket workspaces")
+	}
+
+	var repos []repository
+
+	ctx, cancel := context.WithTimeout(context.Background(), defaultHttpRequestTimeout)
+	defer cancel()
+
+	for _, workspace := range workspaces {
+		logger.Printf("listing repositories in BitBucket workspace: %s", workspace)
+
+		rawRequestURL := bb.APIURL + "/repositories/" + url.PathEscape(workspace) + "?role=member"
+
+		for {
+			body, err := bb.bitbucketAuthenticatedGet(ctx, rawRequestURL)
+			if err != nil {
+				return describeReposOutput{}, errors.Errorf("failed to list repositories in workspace %s: %s", workspace, err)
+			}
+
+			var respObj bitbucketGetProjectsResponse
+			if jErr := json.Unmarshal(body, &respObj); jErr != nil {
+				return describeReposOutput{}, errors.Wrap(jErr, "failed to unmarshal BitBucket repositories response")
+			}
+
+			for _, r := range respObj.Values {
+				if r.Scm == "git" {
+					repo := repository{
+						Name:              r.Name,
+						HTTPSUrl:          "https://bitbucket.org/" + r.FullName + ".git",
+						PathWithNameSpace: r.FullName,
+						Domain:            bitbucketDomain,
+					}
+
+					repos = append(repos, repo)
+				}
+			}
+
+			if respObj.Next != "" {
+				rawRequestURL = respObj.Next
+
+				continue
+			}
+
+			break
+		}
 	}
 
 	return describeReposOutput{
@@ -535,6 +561,7 @@ type BitbucketHost struct {
 	LogLevel             int
 	BackupLFS            bool
 	EncryptionPassphrase string
+	Workspaces           []string
 }
 
 type bitbucketOwner struct {
@@ -563,6 +590,18 @@ type bitbucketGetProjectsResponse struct {
 	Pagelen int                `json:"pagelen"`
 	Values  []bitbucketProject `json:"values"`
 	Next    string             `json:"next"`
+}
+
+type bitbucketWorkspace struct {
+	Slug string `json:"slug"`
+	Name string `json:"name"`
+	UUID string `json:"uuid"`
+}
+
+type bitbucketGetWorkspacesResponse struct {
+	Pagelen int                  `json:"pagelen"`
+	Values  []bitbucketWorkspace `json:"values"`
+	Next    string               `json:"next"`
 }
 
 // return normalised method.
